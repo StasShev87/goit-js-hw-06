1113a199ceb8012bc3bffba453c43b9dfa0cafc0
Stanislav Shevchenko <StasShev87@gmail.com>
2022-10-16 00:05:20 +0300

    Do task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const chColorBtn = document.querySelector("button.change-color");
+const output = document.querySelector("span.color");
+const body = document.querySelector("body");
+
+chColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  output.textContent = randomColor;
+});
