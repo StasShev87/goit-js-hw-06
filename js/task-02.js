bfbd2bd79b5401a131e5185dad26938d9fb84e73
Stanislav Shevchenko <StasShev87@gmail.com>
2022-10-15 22:51:48 +0300

    Do task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const newElements = [];
+// Create new elements
+for (const ingredient of ingredients) {
+  const newElement = document.createElement("li");
+  newElement.textContent = ingredient;
+  newElement.classList.add("item");
+  newElements.push(newElement);
+}
+// Insert the elements
+const ingredientsList = document.querySelector("ul#ingredients");
+ingredientsList.append(...newElements);
