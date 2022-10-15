532dcbb083fc2a35be632d9378a84f160e9f329c
Stanislav Shevchenko <StasShev87@gmail.com>
2022-10-16 00:00:26 +0300

    Do task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector("form.login-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const formData = { email: email.value, password: password.value };
+
+  console.log(formData);
+  event.currentTarget.reset();
+});
